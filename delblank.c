73ced12186334bf18c446970c86e7059846ac1b1
mens <mens@ebensz.com>
2019-02-17 09:34:33 +0800

    fix bug

--- delblank.c
+++ delblank.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>    /* for exit */ 
 #include <getopt.h> 
 #include <ctype.h>
-#include <string.h>
-// char *map="\n\x1\r\x2";
+#include <string.h> // char *map="\n\x1\r\x2";
+#include <unistd.h>
+#include <sys/types.h>
 int trim(char *s, int *lenI, int *chg) { int len = *lenI; int beg=0; int end=0; *chg = 0;//printf("%s\n", s);
     while ( beg < len ) {
         if( !isblank(s[beg])){
@@ -34,37 +35,46 @@ int trim(char *s, int *lenI, int *chg) { int len = *lenI; int beg=0; int end=0; 
     }
     // printf("%s\n", s);
 }
+int trimRevers(char *s, int *lenI, int *chg) { int len = *lenI; int beg=0; int end=len; *chg = 0;//printf("%s\n", s);
+    s[len] = '\n';
+    while(end >= 0){
+        if(s[end] == '\n'){ int end2 = end-1;
+            while(end2 >= 1){
+                if( !isblank(s[end2]) && !isspace(s[end2])){
+                    break; } 
+                //if(s[end2] == '\n'||s[end] == '\r'){
+                    s[end2] = 0;
+                    *chg = 1; //}
+                end2--; }
+            end = end2;
+        }
+        end--; } // printf("%s\n", s);
+    if(*chg){
+        int i =0; int a =0;
+        for(i=0; i<len; i++){
+            if(s[i])
+                s[a++] = s[i];
+        }
+        *lenI = a;
+    }
+}
 #define deB do{ printf("\n%s %d \n", __FUNCTION__, __LINE__);}while(0);
 #define deBV(fmt, ...) do{printf("\n%s %d " fmt "\n", __FUNCTION__, __LINE__,##__VA_ARGS__);}while(0)
 int DelBlank(char *f){
     #define MAX_BUF_LEN 1024*1024*50
-    if(!f && !f[0]) return -1;
-    FILE *pF = fopen(f, "r+");
-    if(pF){ //deB
-        char *s = (char *)malloc(MAX_BUF_LEN);//deB
+    if(!f && !f[0]) return -1; FILE *pF = fopen(f, "r+");
+    if(pF){ char *s = (char *)malloc(MAX_BUF_LEN);//deB
         if(s){ //deB
-            int r = fread(s, 1, MAX_BUF_LEN, pF); //deB
-            int chg=0;
-            trim(s, &r, &chg);
-            if(chg){
-                fseek(pF, 0L, SEEK_SET); printf("%s\n", f);//deBV("%s", f);
-                fwrite(s, 1, r, pF);
-            }
-            free(s);
-        }else{
-            deBV("err malloc");
-        }            
-        fclose(pF); } // trim // while (fgets(buf, MAX_PATH, pF1)) { // 	char *t = NULL; // 	if ((t = strchr(buf, '\n'))) { // 		t[0] = 0; // 	} // 	FILE *pF2 = fopen(buf, "rb"); // 	if (pF2) { // 		strcat(buf, "XXXXXX"); // 		int fd = mkstemp(buf); // 		char buf1[MAX_BUF_LEN] = { 0 }; // 		while (fgets(buf1, MAX_BUF_LEN, pF2)) { // 			if(IsChar(buf1) >= 0){ // 				write(fd, buf1, strlen(buf1)); // 			} // 		} // 		close(fd); // 		fclose(pF2); // 		sprintf(buf1, "%s", buf); // 		buf1[strlen(buf1)-6] = 0; // 		rename(buf, buf1); // 	} // } // fclose(pF1); // unlink(OUT_FILE); 
-}
+            int r = fread(s, 1, MAX_BUF_LEN-1, pF); int chg=0;//deB
+            if(r>0) trimRevers(s, &r, &chg);//trim(s, &r, &chg);
+            if(chg){ fseek(pF, 0L, SEEK_SET); printf("%s\n", f);//deBV("%s", f);
+                fwrite(s, 1, r, pF); truncate(f, r); } free(s); }else{ deBV("err malloc"); } fclose(pF); } }// trim // while (fgets(buf, MAX_PATH, pF1)) { // 	char *t = NULL; // 	if ((t = strchr(buf, '\n'))) { // 		t[0] = 0; // 	} // 	FILE *pF2 = fopen(buf, "rb"); // 	if (pF2) { // 		strcat(buf, "XXXXXX"); // 		int fd = mkstemp(buf); // 		char buf1[MAX_BUF_LEN] = { 0 }; // 		while (fgets(buf1, MAX_BUF_LEN, pF2)) { // 			if(IsChar(buf1) >= 0){ // 				write(fd, buf1, strlen(buf1)); // 			} // 		} // 		close(fd); // 		fclose(pF2); // 		sprintf(buf1, "%s", buf); // 		buf1[strlen(buf1)-6] = 0; // 		rename(buf, buf1); // 	} // } // fclose(pF1); // unlink(OUT_FILE); 
 int main1(char *fL) {  //deBV("%s", fL);
 	char c = '\r'; c = '\n';
     #define MAX_LEN 4096
-	char b[MAX_LEN] = {0}; char r[MAX_LEN] = {0};
-    sprintf(b, "dos2unix %s 2>/dev/null", fL);
-    system(b);
+	char r[MAX_LEN] = {0};//char b[MAX_LEN] = {0}; sprintf(b, "dos2unix %s 2>/dev/null", fL); system(b);
     FILE *pF = fopen(fL, "rb");
-    if(pF){
-        char *t = 0;//fgets(r, MAX_LEN, pF);
+    if(pF){ char *t = 0;//fgets(r, MAX_LEN, pF);
         while(fgets(r, MAX_LEN, pF)){
             int l = strlen(r);
             if(r[l-1]=='\n')
