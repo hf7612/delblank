5b48c50ddd3d3032f98826f47614ed8c144790f2
hf7612 <hf7612@163.com>
2019-10-01 12:10:33 +0800

    optimize

--- access_time_get.c
+++ access_time_get.c
@@ -9,11 +9,11 @@ int main(int argc, char *argv[]){ const char *pathname;// = argv[1];
         if(!strcmp(argv[1], "-f")){ pathname = argv[2];
             if(!stat(pathname, &statbuf)){
                 if(memcmp(&statbuf.st_atim, &statbuf.st_mtim, sizeof(statbuf.st_atim))){// if(statbuf.st_atim == statbuf.st_mtim){
-                    printf(argv[2]); printf("\n"); }}
+                    printf(pathname); printf("\n"); }}
         }else printf(" var err \n");
-    }else if(argc == 2){
-        FILE *pF = fopen(argv[2], "r");
-        if(pF){ char buf[MAX_PATH]; buf[0] = 0; char *pS; pathname = argv[2];
+    }else if(argc == 2){ pathname = argv[1];
+        FILE *pF = fopen(pathname, "r");
+        if(pF){ char buf[MAX_PATH]; buf[0] = 0; char *pS; pathname = pathname;
             if(!stat(pathname, &statbuf)){
                 if(statbuf.st_size > 0){ pS = malloc(statbuf.st_size+1);
                     if(pS){ pS[statbuf.st_size] = 0; int r = fread(pS, 1, statbuf.st_size, pF);
