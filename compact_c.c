a909ef2531d2128760977bd24690c5a49fdfba2a
zhuanmen.hu <zhuanmen.hu@tcl.com>
2019-10-15 11:39:00 +0800

    compact_c optimize

--- compact_c.c
+++ compact_c.c
@@ -191,6 +191,15 @@ static int compact3(char *s, int *lenI, int *chg) { int len = *lenI; int beg=0; 
         t[0] = ' ';
         sR = t+offL;
         *chg = 1; }}// compact &	
+static int compact4(char *s, int *lenI, int *chg) { int len = *lenI; int beg=0; int end=0; //*chg = 0; //deB//printf("%s\n", s);
+    char *t = 0;//g ets(s);
+    char *t2 = 0; int ind0=0,ind1=0,ind2=0,ind3=0;
+    char * SYM4 = "\nextern ";
+    char *sR = s; int offL = strlen(SYM4);
+    while(t = strstr(sR, SYM4) ){ //printf(" %d ", t-s);
+        t[0] = ' ';
+        sR = t+offL;
+        *chg = 1; }}
 // static int compact4(char *s, int *lenI, int *chg) { int len = *lenI; int beg=0; int end=0; //*chg = 0; //deB//printf("%s\n", s);
 //     char *t = 0;//g ets(s);
 //     char *t2 = 0; int ind0=0,ind1=0,ind2=0,ind3=0; // char * SYM4 = "&";
@@ -239,7 +248,7 @@ static int trimLargeBlock(char *f){ struct stat statbuf; //deB
                     compact1(gS, &r, &chg);
                     compact2(gS, &r, &chg);
                     compact3(gS, &r, &chg);
-                    // compact4(gS, &r, &chg);
+                    compact4(gS, &r, &chg);
                     if(chg){// do_md5(gS, r);
                         fseek(pF, 0L, SEEK_SET); printf(" %s\n", f);//deBV("%gS", f);
                         fwrite(gS+1, 1, r, pF); } // int ftruncate(int fd, off_t length); // printf(" %s ", gS); // fwrite(gS, 1, statbuf.st_size, stdout);
